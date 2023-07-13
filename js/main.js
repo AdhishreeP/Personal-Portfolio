796662b8f48734136bf0f14b8d041abf3e11abfb
AdhishreeP <93819684+AdhishreeP@users.noreply.github.com>
2023-07-13 11:10:31 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+//loader
+var loader = document.getElementById("loader");
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
 
 // Header Scroll
 let nav = document.querySelector(".navbar");
